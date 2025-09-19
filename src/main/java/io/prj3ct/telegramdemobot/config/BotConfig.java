3e7467b362338aea249bfa104beede33a72ecd14
ArtemBurlachkov <ArtemJavaRush@yandex.ru>
2025-09-19 11:07:05 +0300

    Fix(ci): Use 'docker compose' instead of 'docker-compose'

--- src/main/java/io/prj3ct/telegramdemobot/config/BotConfig.java
+++ src/main/java/io/prj3ct/telegramdemobot/config/BotConfig.java
@@ -1,18 +1,20 @@
 package io.prj3ct.telegramdemobot.config;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
-@Data
-@PropertySource("application.properties")
+@ConfigurationProperties(prefix = "bot")
+@Getter
+@Setter
 public class BotConfig {
 
-    @Value("${bot.name}")
-    String botName;
+    private String name;
 
-    @Value("${bot.token}")
-    String botToken;
+    private String token;
 }
